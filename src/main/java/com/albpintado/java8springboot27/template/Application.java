eb398afb90ba62d981fb267dd5a7e9fd7dedee56
Pintado <alberto.pintado@solera.com>
2022-11-27 17:31:07 +0100

    First version of Spring MySQL Template

---
+++ src/main/java/com/albpintado/java8springboot27/template/Application.java
@@ -0,0 +1,13 @@
+package com.albpintado.java8springboot27.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/albpintado/java8springboot27/template/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albpintado.java8springboot27.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
